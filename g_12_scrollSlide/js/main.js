2f0c9d4c5bf2bb628af433163cb335eb78475209
ejonghwan <sun87-1@daum.net>
2023-05-31 12:50:59 +0900

    구조분해할당 스프레드연산자

--- g_12_scrollSlide/js/main.js
+++ g_12_scrollSlide/js/main.js
@@ -1,21 +1,57 @@
-const fig = document.querySelector('figure');
-let imgs;
+const mask = document.querySelector('aside');
+const delay = convertSpeed(mask);
+const target = 'figure';
+const num = 200;
 
-for(let i = 0; i < 200; i++) {
-    imgs += `
-        <img src="img/pic${i}.jpg" alt="" />
-    `
+//figure에 안쪽에 동적으로 200개 이미지 돔생성해서 리턴하는 함수 호출
+//동적으로 돔을 생성하자마자 리턴해주는 요소를 바로 변수에 담음
+const imgDOM = createImgs(target, num);
+
+let count = 0;
+
+imgDOM.forEach((img) => {
+	img.onload = () => {
+		count++;
+		const percent = parseInt((count / 200) * 100);
+		mask.querySelector('p').innerHTML = percent + '%';
+		mask.querySelector('.bar').style.width = percent + '%';
+
+		if (percent === 100) {
+			console.log('이미지 소스 로딩 완료');
+			mask.classList.add('off');
+
+			setTimeout(() => {
+				mask.remove();
+			}, delay);
+		}
+	};
+	img.onerror = () => {
+		console.log('이미지 출력 중 에러');
+	};
+});
+
+//마우스무브시 포인터 좌표값 200분율로 변경
+window.addEventListener('mousemove', (e) => matchMove(imgDOM, num, e));
+
+//동적 이미지 생성 함수
+function createImgs(target, num) {
+	const frame = document.querySelector(target);
+	let imgs = '';
+	for (let i = 0; i < num; i++) {
+		imgs += `<img src='img/pic${i}.jpg' />`;
+	}
+	frame.innerHTML = imgs;
+	//DOM생성하자마자 바로 활용할수있도록 생성된 요소를 내보내주는 코드
+	return frame.querySelectorAll('img');
 }
 
-fig.innerHTML = imgs
-const imgDOM = fig.querySelectorAll('img')
-// console.log(imgDOM)
+//마우스 포인터 위치와 이미지 순서 매칭 함수
+function matchMove(arrEl, num, e) {
+	const percent = parseInt((e.clientX / window.innerWidth) * num);
+	for (const img of arrEl) img.style.display = 'none';
+	arrEl[percent].style.display = 'block';
+}
 
-window.addEventListener('mousemove', e => {
-    const percent = parseInt(e.clientX / window.innerWidth * 200);
-    // console.log(percent)
-
-    for(const img of imgDOM) img.style.display = "none";
-    imgDOM[percent].style.display = "block"
-    // console.log(imgDOM)
-})
+function convertSpeed(el) {
+	return parseFloat(getComputedStyle(el).transitionDuration) * 1000;
+}
