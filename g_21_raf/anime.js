7b9f3e0c43723127719f7e7e14cc91181d8b347d
ejonghwan <sun87-1@daum.net>
2023-06-21 12:45:18 +0900

    raf ani 작성

--- g_21_raf/anime.js
+++ g_21_raf/anime.js
@@ -1,37 +1,62 @@
-/*
-  peformance.now();
-  1ms단위로 정밀한 시간계산이 가능
-  브라우저가 로딩된 순간부터 해당 구문이 호출된 시점까지의 시간을 ms단위로 반환
-  정밀한 시간계산이 필요할때 활용됨
-*/
 const btn = document.querySelector('button');
 const box = document.querySelector('#box');
-let num = 0;
-let startTime = 0;
-
-
-// 횟수를 조절할수없으니 시간을 정해줌 = speed
-let speed = 1000;  
-const targetValue = 500;
 
 btn.addEventListener('click', () => {
-	startTime = performance.now();
-	// console.log('시작시간', startTime);
-	requestAnimationFrame(move);
+	anime(window, {
+		prop: 'scroll',
+		value: 1000,
+		duration: 500,
+	});
 });
 
-function move(time) {
-	// console.log('반복사이클 마다의 누적시간', time);
+function anime(selector, option) {
+	const startTime = performance.now();
+	let currentValue = null;
 
-	let timeLast = time - startTime;
-	let progress = timeLast / speed
-		
-	console.log(progress)
+	//option객체의 prop속성명이 scroll이면 scrollY값을 활용하고 그렇지 않은 나머지 경우에는 getComputedStyle로 스타일값을 가져옴
+	option.prop === 'scroll' ? (currentValue = selector.scrollY) : (currentValue = parseFloat(getComputedStyle(selector)[option.prop]));
 
+	//만약 value속성으로 받은 값이 문자열이면 퍼센트연산처리 해야되므로 정수가 아닌 실수로 값을 변환
+	const isString = typeof option.value;
+	if (isString === 'string') {
+		//option.value값이 문자열이면 기존의 currentValue값도 퍼센트 처리
+		//퍼센트로 값을 변환하기 위해서는 부모요소의 전체넓이, 전체 높이값을 구함
+		const parentW = parseInt(getComputedStyle(selector.parentElement).width);
+		const parentH = parseInt(getComputedStyle(selector.parentElement).height);
 
-	// 매 반복 횟수마다 현재 걸리는 누적시간값을 전체시간으로 나누면 0~1사이의 실수로 반환가능
-	if(progress < 1 ) {
-		progress = 1
-		requestAnimationFrame(move);
+		//가로나 세로축으로 퍼센트로 적용될만한 속성명을 배열로 그룹화
+		const x = ['left', 'right', 'width'];
+		const y = ['top', 'bottom', 'height'];
+		const errProps = ['margin-left', 'margin-right', 'padding-left', 'padding-right', 'margin-top', 'margin-bottom', 'padding-top', 'padding-bottom'];
+
+		//퍼센트로 적용할수 없는 속성값이 들어오면 경고문구 출력하고 종료
+		for (let cond of errProps) if (option.prop === cond) return console.error('margin, padding값은 퍼센트 모션처리할 수 없습니다.');
+
+		//option.prop값으로 위의 배열로 설정한 속성이 들어오면 currentValue값을 부모요소의 크기대비 퍼센트로 변환
+		for (let cond of x) option.prop === cond && (currentValue = (currentValue / parentW) * 100);
+		for (let cond of y) option.prop === cond && (currentValue = (currentValue / parentH) * 100);
+
+		option.value = parseFloat(option.value);
+	}
+
+	option.value !== currentValue && requestAnimationFrame(run);
+
+	function run(time) {
+		let timelast = time - startTime;
+		let progress = timelast / option.duration;
+
+		progress < 0 && (progress = 0);
+		progress > 1 && (progress = 1);
+		progress < 1 ? requestAnimationFrame(run) : option.callback && option.callback();
+
+		let result = currentValue + (option.value - currentValue) * progress;
+
+		//속성값이 퍼센트이면 적용도 퍼센트 처리 그렇지 않으면 그냥 px처리
+		if (isString === 'string') selector.style[option.prop] = result + '%';
+		//속성명이 opacity이면 실수값을 그대로 스타일에 적용
+		else if (option.prop === 'opacity') selector.style[option.prop] = result;
+		//속성명이 scroll이면 window의 scroll값 적용
+		else if (option.prop === 'scroll') selector.scroll(0, result);
+		else selector.style[option.prop] = result + 'px';
 	}
 }
